dc995d2b3054183015ab7f6190d5125ede3d5e55
Alphonso Darel <alphonsodarely@gmail.com>
2025-02-05 15:38:03 +0100

    first commit

---
+++ src/01/App01.tsx
@@ -0,0 +1,17 @@
+//mon premier composant
+
+import logo  from './image.jpg'
+
+export default function App01(){
+    return(
+
+
+        
+        <div className="ady" >
+            <div>
+                <img src={logo} alt="" width={500} />
+            </div>
+            my first component
+        </div>
+    )
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import './01/style.css'
+import { createRoot } from 'react-dom/client'
+
+import App01 from './01/App01.tsx'
+
+
+
+createRoot(document.getElementById('root')!).render(
+  
+    <App01 />
+  
+)
